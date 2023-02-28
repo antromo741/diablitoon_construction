2509349c8f7fc523fa73d8dd1d3b275e2ca8998f
Anthony Romagnolo <anthonyroma741@gmail.com>
2023-02-28 18:28:14 -0500

    remove bad assets, fix styling, -need to fix mobile view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { useCallback } from "react";
-import videoBg from "./assets/toonwalk.mp4"
+import videoBg from "./assets/toonwalk.mp4";
+import twitterIcon from "./assets/twittervector.svg";
+import discordIcon from "./assets/discordvector.svg";
+import instagramIcon from "./assets/instagram.svg";
+import openseaIcon from "./assets/opensea.svg";
+import headLogo from "./assets/group.svg"
 
 function App() {
   const onTwitterContainerClick = useCallback(() => {
@@ -10,82 +14,66 @@ function App() {
 
   return (
     <div className="App">
-      <div className='home-construction-2"'>
-        <header className="App-header">
-          <div className="x1080-whitebg-gif-1-group">
-            {/* The video background to play with sound the user needs to click*/}
-            <video className="x1080-whitebg-gif-11" src={videoBg} autoPlay loop muted />
-            {/* Social media */}
-            <div className="social-header1">
-              <div className="discord1">
-                <img className="vector-icon2" alt="" src="../vector.svg" />
-              </div>
-              <div className="twitter1" onClick={onTwitterContainerClick}>
-                <img className="vector-icon3" alt="" src="../vector3.svg" />
-              </div>
-              <img className="instagram-icon1" alt="" src="../instagram1.svg" />
-              <img className="opensea-icon1" alt="" src="../opensea1.svg" />
-            </div>
-            {/* Logo and the top nav bar */}
-            <div className="logo-head1">
-              <img className="group-icon1" alt="" src="../group1.svg" />
-            </div>
-            <div className="header-main1">
-              <div className="header-group">
-                <div className="header1">
-                  <div className="home2">
-                    <div className="home3">Home</div>
-                  </div>
-                  <div className="home2">
-                    <div className="home3">{`About `}</div>
-                  </div>
-                  <div className="home2">
-                    <div className="home3">Goals</div>
-                  </div>
-                  <div className="home2">
-                    <div className="home3">Team</div>
-                  </div>
-                  <div className="home2">
-                    <div className="home3">Facts</div>
-                  </div>
-                  <div className="medium2">
-                    <div className="home3">Medium</div>
-                  </div>
-                </div>
-                <div className="header-button1">
-                  <div className="home3">Connect wallet</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        {/* add body */}
-        <div className="diablitoons-is3">
-          <div className="diablitoons-is4">
-            <div className="diablitoons-is5">{`Diablitoons is `}</div>
-          </div>
-          {/* Need to set up the revolving wheel and use these as props */}
-          <div className="mezcal1">Mezcal</div>
-          <div className="party1">Party</div>
-          <div className="tacos1">Tacos</div>
-          <div className="family1">Family</div>
-          <div className="you1">YOU.</div>
-          <div className="art1">Art</div>
-        </div>
+      <div className='home-construction-2'>
+      {/* App Header */}
+      <header>
+        <div className='nav-container'>
 
-      </div>
+          {/* Nav logo */}
+          <div className="logo-head">
+            <img className="head-logo" alt="" src={headLogo} />
+          </div>
+
+          <div className="nav-main">
+            <div className="nav-group">
+
+              {/* Nav titles */}
+              <div className="nav-text">
+                <div>Home</div>
+                <div>{`About`}</div>
+                <div>Goals</div>
+                <div>Team</div>
+                <div>Facts </div>
+                <div> Medium</div>
+              </div>
+
+              {/* Connect wallet */}
+              <div className="connect-wallet">
+                <div className="home">Connect wallet</div>
+              </div>
+            </div>
+          </div>
+
+          {/* Social media */}
+          <div className="social-header">
+            <img className="discord-icon" alt="" src={discordIcon} />
+            <img className="twitter-icon" alt="" src={twitterIcon} onClick={onTwitterContainerClick} />
+            <img className="instagram-icon" alt="" src={instagramIcon} />
+            <img className="opensea-icon" alt="" src={openseaIcon} />
+          </div>
+
+        </div>
+      </header>
+
+      <main>
+        {/* The video background, need to add an icon for aound*/}
+        {/*  <video className="" src={videoBg} autoPlay loop muted /> */}
+        <section>
+          <div className="diablitoons-text-container">
+            <div className='diablitoons-text-main'>
+              <div className="diablitoons-text-start">{`Diablitoons is `}</div>
+            </div>
+            {/* Need to set up the revolving wheel and use these as props */}
+            <div className="mezcal1">Mezcal</div>
+           {/*  <div className="party1">Party</div>
+            <div className="tacos1">Tacos</div>
+            <div className="family1">Family</div>
+            <div className="you1">YOU.</div>
+            <div className="art1">Art</div> */}
+          </div>
+        </section>
+      </main>
+    </div>
     </div>
   );
 }
