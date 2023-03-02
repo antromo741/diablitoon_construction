2bc8cc35ddef77549bd4e0103cac9d20b6be7418
Anthony Romagnolo <anthonyroma741@gmail.com>
2023-03-02 13:27:18 -0500

    the text works well and looks good -need to add mobile view for the text. -need to fix black screen flashing at the end of the video -commented out code for video in animatedtext component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import './App.css';
 import ToonNavbar from './components/navbar/ToonNavbar';
 import AnimatedText from './components/animatedText/AnimatedText';
+import { useState } from 'react'
+import videoWalk from './assets/toonwalk.mp4'
 
 function App() {
+  const [showText, setShowText] = useState(false)
 
+  const handleVideoEnd = () => {
+    setShowText(true);
+    document.getElementById("video-container").style.zIndex = "-1";
+  };
 
   return (
     <div className="App">
       <div className='main-page'>
-        {/* App Header */}
-        <ToonNavbar />
-        <AnimatedText />
+        {showText ? (
+          <div className={showText ? "textVisible" : "text"}>
+            <ToonNavbar />
+            <AnimatedText />
+          </div>
+        ) : (
+          <video
+            className={showText ? "videoHidden" : "video"}
+            src={videoWalk}
+            autoPlay
+            muted
+            onEnded={handleVideoEnd}
+            style={{ width: '100vw', height: '100vh', objectFit: 'cover' }}
+          />
+        )}
       </div>
     </div>
   );
 }
 
 export default App;
-  /* The video background, need to add an icon for aound*/
-/*  <video className="" src={videoBg} autoPlay loop muted /> */
--- src/components/animatedText/AnimatedText.jsx
+++ src/components/animatedText/AnimatedText.jsx
@@ -4,23 +4,41 @@ import { useState, useEffect } from 'react'
 
 const AnimatedText = () => {
   const [wordIndex, setWordIndex] = useState(0)
+  const [showCookingText, setShowCookingText] = useState(false)
 
-  const words = ['Mezcal', 'Party', 'Tacos', 'Family', 'YOU.', 'Art']
+  const words = ['Art', 'Mezcal', 'Party', 'Tacos', 'Family', 'YOU.']
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setWordIndex((prevIndex) => (prevIndex + 1) % words.length)
+      if (wordIndex === words.length - 1) {
+        setShowCookingText(true)
+        setTimeout(() => {
+          setShowCookingText(false)
+          setWordIndex(0)
+        }, 2000)
+      } else {
+        setWordIndex((prevIndex) => prevIndex + 1)
+      }
     }, 1000)
     return () => clearInterval(interval)
-  }, [words.length])
+  }, [wordIndex, words.length])
 
   return (
     <main>
       <section>
         <div className="diablitoons-text-container">
           <div className="diablitoons-text-main">
-            <div className="diablitoons-text-start">{`Diablitoons is `}</div>
-            <div className="mezcal1">{`${words[wordIndex]}`}</div>
+            <div
+              className={` ${
+                showCookingText ? 'cooking-text-1' : 'diablitoons-text-start'
+              }`}
+            >
+              {showCookingText ? 'Cooking' : `Diablitoons is`}
+            </div>
+
+            <div className={`${showCookingText ? 'cooking-text' : 'mezcal1'}`}>
+              {showCookingText ? 'up juicy stuff' : words[wordIndex]}
+            </div>
           </div>
         </div>
       </section>
@@ -29,3 +47,77 @@ const AnimatedText = () => {
 }
 
 export default AnimatedText
+
+/* import React from 'react'
+import './animatedText.css'
+import { useState, useEffect } from 'react'
+import videoWalk from '../../assets/toonwalk.mp4'
+
+const AnimatedText = () => {
+  const [showText, setShowText] = useState(false)
+
+  const handleVideoEnd = () => {
+    setShowText(true)
+  }
+
+  const [wordIndex, setWordIndex] = useState(0)
+  const [showCookingText, setShowCookingText] = useState(false)
+
+  const words = ['Art', 'Mezcal', 'Party', 'Tacos', 'Family', 'YOU.']
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (wordIndex === words.length - 1) {
+        setShowCookingText(true)
+        setTimeout(() => {
+          setShowCookingText(false)
+          setWordIndex(0)
+        }, 2000)
+      } else {
+        setWordIndex((prevIndex) => prevIndex + 1)
+      }
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [wordIndex, words.length])
+
+  return (
+    <>
+      {showText ? (
+        <main>
+          <section>
+            <div className="diablitoons-text-container">
+              <div className="diablitoons-text-main">
+                <div
+                  className={` ${
+                    showCookingText
+                      ? 'cooking-text-1'
+                      : 'diablitoons-text-start'
+                  }`}
+                >
+                  {showCookingText ? 'Cooking' : `Diablitoons is`}
+                </div>
+
+                <div
+                  className={`${showCookingText ? 'cooking-text' : 'mezcal1'}`}
+                >
+                  {showCookingText ? 'up juicy stuff' : words[wordIndex]}
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+      ) : (
+        <video
+          src={videoWalk}
+          autoPlay
+          muted
+          onEnded={handleVideoEnd}
+          style={{ width: '100vw', height: '100vh', objectFit: 'cover' }}
+        />
+      )}
+    </>
+  )
+}
+
+export default AnimatedText
+ */
