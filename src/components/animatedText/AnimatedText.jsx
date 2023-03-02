28ecf867e4f9938b2f0d9959a7b6ddf0e2df3a19
Anthony Romagnolo <anthonyroma741@gmail.com>
2023-03-02 12:01:22 -0500

    styled and centered text added animation interval need to remove background colors and add the video

--- src/components/animatedText/AnimatedText.jsx
+++ src/components/animatedText/AnimatedText.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import './animatedText.css'
+import { useState, useEffect } from 'react'
 
 const AnimatedText = () => {
+  const [wordIndex, setWordIndex] = useState(0)
+
+  const words = ['Mezcal', 'Party', 'Tacos', 'Family', 'YOU.', 'Art']
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setWordIndex((prevIndex) => (prevIndex + 1) % words.length)
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [words.length])
+
   return (
     <main>
-          <section>
-            <div className="diablitoons-text-container">
-              <div className='diablitoons-text-main'>
-                <div className="diablitoons-text-start">{`Diablitoons is `}</div>
-              </div>
-              {/* Need to set up the revolving wheel and use these as props */}
-              <div className="mezcal1">Mezcal</div>
-            </div>
-          </section>
-        </main>
+      <section>
+        <div className="diablitoons-text-container">
+          <div className="diablitoons-text-main">
+            <div className="diablitoons-text-start">{`Diablitoons is `}</div>
+            <div className="mezcal1">{`${words[wordIndex]}`}</div>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
