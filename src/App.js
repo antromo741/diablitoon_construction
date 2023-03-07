57a40aab0ad9df0ee4bc16a1b483df5e3f065ebc
Anthony Romagnolo <anthonyroma741@gmail.com>
2023-03-07 14:31:21 -0500

    add nw gif -add sizing for mobile and web view -make the view scrollable -fixed sizing of main-page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import videoWalk from './assets/introvid.mp4';
+import toon from './assets/toon.gif'
 import ToonNavbar from './components/navbar/ToonNavbar';
 import AnimatedText from './components/animatedText/AnimatedText';
 
@@ -20,6 +21,11 @@ function App() {
         <div className={showText ? "textVisible" : 'textHidden'}>
           <AnimatedText />
         </div>
+
+        <div className="toon-gif-container">
+          <img src={toon} alt="toon-gif" className="toon-gif" />
+        </div>
+
         <div >
           <video className={showText ? "videoHidden" : "video"}
             src={videoWalk}
